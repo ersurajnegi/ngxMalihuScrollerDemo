901eb1b2ad361ca349218f28087438bc87baffab
SUraj Negi <er.surajnegi@gmail.com>
2017-07-24 01:06:51 +0530

    completed the Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
+import { text } from './text';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  text = text;
+  options = {
+    theme: "dark-thick",
+    axis: "y"
+  }
+  themesArray = [
+    { theme: "light", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+    { theme: "dark", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+    { theme: "minimal", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+    { theme: "minimal-dark", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+    { theme: "dark-2", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+    { theme: "light-2", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+    { theme: "dark-3", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+    { theme: "light-3", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+
+
+    { theme: "light-thick", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+    { theme: "dark-thick", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+    { theme: "light-thin", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+    { theme: "dark-thin", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+
+
+    { theme: "inset-dark", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+    { theme: "inset", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+    { theme: "inset-2-dark", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+    { theme: "inset-2", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+
+
+    { theme: "inset-3", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+    { theme: "inset-3-dark", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+    { theme: "rounded", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+    { theme: "rounded-dark", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+
+    { theme: "rounded-dots-dark", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+    { theme: "rounded-dots", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+    { theme: "3d-dark", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } },
+    { theme: "3d", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+
+
+    { theme: "3d-thick", axis: "y", customClass: "custom-dark", scrollButtons: { enable: true } },
+    { theme: "3d-thick-dark", axis: "y", customClass: "custom-light", scrollButtons: { enable: true } }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MalihuCustomScrollerModule } from 'ngx-malihu-scroller';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MalihuCustomScrollerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
